94c2a637440a388d54aecba37312c6c0719babe2
franck34 <franck.tab@gmail.com>
2019-01-05 11:41:11 +0100

    change output for show dbs

--- src/commands/show.js
+++ src/commands/show.js
@@ -15,7 +15,11 @@ const databases = require('../databases');
  function show(options, callback) {
      if (options.params === 'dbs') {
          let dbs = databases.listSync();
-         options.socket.write(JSON.stringify({result:dbs})+ENV.NET_TCP_EOF);
+         let i = dbs.length;
+         while (i--) {
+             options.socket.write(dbs[i]+ENV.NET_TCP_EOF);
+         }
+         options.socket.write(ENV.NET_TCP_PROMPT);
          return;
      }
      callback(new Error('missing or bad parameters (dbs)'));
