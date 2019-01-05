7c708ae06492fb143c160430f89b192544225158
franck34 <franck.tab@gmail.com>
2019-01-05 11:26:53 +0100

    add command "db"

---
+++ src/commands/db.js
@@ -0,0 +1,20 @@
+const ENV = require('../env');
+
+/**
+ * Client ask for currently selected database
+ *
+ * @example
+ * > db
+ * test
+ *
+ * @param {object} options - options.command, options.params. options.socket
+ * @param {function} callback - callback
+ * @memberof Commands
+ */
+ function quit(options, callback) {
+     options.socket.write(options.socket.loky.currentDatabase+ENV.NET_TCP_EOF);
+     options.socket.end();
+     callback();
+}
+
+module.exports = quit;
