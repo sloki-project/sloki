ca820c7c2bdfe001b03edd0077697756df2ce919
franck34 <franck.tab@gmail.com>
2019-01-13 12:43:42 +0100

    refactor

--- index.js
+++ index.js
@@ -1,63 +1,12 @@
-const databases = require('./src/databases');
-const tcp = require('./src/transports/tcp');
-const http = require('./src/transports/http');
 const log = require('evillogger')({ns:'main'});
-const path = require('path');
-
-let closing = false;
-
-function start(callback) {
-    tcp.start((err) => {
-        if (err) {
-            callback && callback(err);
-            return;
-        }
-
-        http.start((err) => {
-            if (err) {
-                callback && callback(err);
-                return;
-            }
-
-            process.on('SIGINT', () => {
-                if (closing) {
-                    log.info('SIGINT received, forcing exit');
-                    process.exit();
-                    return;
-                }
-
-                closing = true;
-                log.info('SIGINT received');
-                stop();
-            });
-
-            callback && callback();
-        })
-    });
-}
-
-function stop(callback) {
-    log.warn("shutdown in progress");
-    tcp.stop((err) => {
-        http.stop((err) => {
-            //setTimeout(() => {
-                log.info("exiting ...");
-                if (callback) {
-                    callback(err);
-                    return;
-                }
-
-                process.exit(err ? 1 : 0);
-            //},2000);
-        });
-    });
-}
+const server = require('./src/server');
 
 if (module === require.main) {
-    start();
+    server.start(() => {
+        log.info("server started");
+    });
 }
 
 module.exports = {
-    start:start,
-    stop:stop
+    //client:require('./src/client')
 }
