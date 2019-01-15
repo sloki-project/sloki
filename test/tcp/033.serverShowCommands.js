07c988ff15421ac532a05803ef836e23cd29b6e2
franck34 <franck.tab@gmail.com>
2019-01-15 19:33:55 +0100

    showCommands test

---
+++ test/tcp/033.serverShowCommands.js
@@ -0,0 +1,38 @@
+const tap = require('../tap');
+const use = require('abrequire');
+const endpoint = require('../endpoints').tcp;
+const commandsList = use('src/commands').list;
+
+const Client = use('src/Client');
+
+let tcpClient = new Client(endpoint);
+
+tap.test(
+    __filename,
+    {timeout:500},
+    (t) => {
+        tcpClient
+            .connect()
+            .then(() => {
+                t.test(
+                    "showCommands",
+                    (tShowCommands)  => {
+                        tcpClient.showCommands((err, result) => {
+                            t.deepEqual(err, undefined, 'should not return an error');
+                            
+                            // example
+                            // [ 'getMaxClients', 'quit', 'setMaxClients', ... ]
+                            t.deepEqual(
+                                result,
+                                Object.keys(commandsList),
+                                "should return all available commands"
+                            );
+                            tShowCommands.end();
+                            t.end();
+                            process.exit(0);
+                        });
+                    }
+                )
+            })
+    }
+);
