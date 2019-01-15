d87bb61d1b994b05533bd2d9a8a8239967bbc747
franck34 <franck.tab@gmail.com>
2019-01-15 19:28:58 +0100

    cosmetic

--- test/tcp/032.serverShowClients.js
+++ test/tcp/032.serverShowClients.js
@@ -3,10 +3,8 @@ const use = require('abrequire');
 const endpoint = require('../endpoints').tcp;
 
 const Client = use('src/Client');
-const ENV = use('src/env');
 
 let tcpClient = new Client(endpoint);
-let maxClients = ENV.NET_TCP_MAX_CLIENTS;
 
 tap.test(
     __filename,
@@ -21,7 +19,11 @@ tap.test(
                         tcpClient.showClients((err, result) => {
                             // example
                             // [ '127.0.0.1:1619' ]
-                            t.equal(result.length,1,"should return only one connected client ");
+                            t.equal(
+                                result.length,
+                                1,
+                                "should return only one connected client
+                            ");
                             tShowClients.end();
                             t.end();
                             process.exit(0);
