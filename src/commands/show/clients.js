8575249bfe429e7ee8737cb2c5c3546ac9a423c0
franck34 <franck.tab@gmail.com>
2019-01-15 19:22:54 +0100

    refactor showClients

--- src/commands/show/clients.js
+++ src/commands/show/clients.js
@@ -11,44 +11,12 @@ require('moment-duration-format');
  * > show clients
  * ...
  *
- * @param {object} options - options.command, options.params. options.socket
+ * @param {object} params - not used
  * @param {function} callback - callback
  * @memberof Commands
  */
-function showClients(options, callback) {
-    let currentClients = Object.keys(options.socket.pool);
-    let clients = {};
-    let clientId;
-
-    for (let i = 0;i<currentClients.length;i++) {
-        clientId = currentClients[i];
-        clients[clientId] = {
-            uptime:moment
-                    .duration(options.socket.pool[clientId].getUptime(), "ms")
-                    .format("d[d] h[h]:m[m]:s[s]")
-        }
-    }
-
-    if (options.socket.getOutputFormat() === "json") {
-        options.socket.write(used,{prompt:true});
-        callback && callback();
-        return;
-    }
-
-    if (options.socket.getOutputFormat() === "text") {
-        for (let cid in clients) {
-            options.socket.write(
-                sprintf(
-                    "%-10s %20s",
-                    cid,
-                    clients[cid].uptime
-                )
-            );
-        }
-        options.socket.prompt();
-        callback && callback();
-        return;
-    }
+function showClients(params, callback) {
+    callback(null, Object.keys(this.server.clients));
 }
 
 module.exports = showClients;
