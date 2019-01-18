9494703e72946576185e5d62957199f32dda85a4
franck34 <franck.tab@gmail.com>
2019-01-18 10:34:26 +0100

    add getCollection test

--- test/tcp/331.database.addCollectionWithOptions.js
+++ test/tcp/331.database.addCollectionWithOptions.js
@@ -2,6 +2,59 @@ let dbName = "__testAddCollectionWithOptions";
 let collectionName = "myCollection";
 let collectionOptions = {unique:['uid']};
 
+let expectedCollectionProperties = {
+    "name":"myCollection",
+    "data":[],
+    "idIndex":[],
+    "binaryIndices":{},
+    "constraints":{
+        "unique":{
+            "uid":{
+                "field":"uid",
+                "keyMap":{},
+                "lokiMap":{}
+            }
+        },
+        "exact":{}
+    },
+    "uniqueNames":["uid"],
+    "transforms":{},
+    "objType":"myCollection",
+    "dirty":true,   // collection has just been created, considered as dirty
+    "cachedIndex":null,
+    "cachedBinaryIndex":null,
+    "cachedData":null,
+    "adaptiveBinaryIndices":true,
+    "transactional":false,
+    "cloneObjects":false,
+    "cloneMethod":"parse-stringify",
+    "asyncListeners":false,
+    "disableMeta":false,
+    "disableChangesApi":true,
+    "disableDeltaChangesApi":true,
+    "autoupdate":false,
+    "serializableIndices":true,
+    "ttl":{
+        "age":null,
+        "ttlInterval":null,
+        "daemon":null
+    },
+    "maxId":0,
+    "DynamicViews":[],
+    "events":{
+        "insert":[],
+        "update":[],
+        "pre-insert":[],
+        "pre-update":[],
+        "close":[],
+        "flushbuffer":[],
+        "error":[],
+        "delete":[null],
+        "warning":[null]
+    },
+    "changes":[]
+}
+
 require('./client')(__filename, (test, client) => {
     test.test("use", (subtest)  => {
         client.use(dbName, (err, result) => {
@@ -28,4 +81,11 @@ require('./client')(__filename, (test, client) => {
         });
     });
 
+    test.test("getCollection", (subtest)  => {
+        client.getCollection(collectionName, (err, result) => {
+            subtest.deepEqual(err, undefined, 'command should not return an error');
+            subtest.deepEqual(result, expectedCollectionProperties, "should return "+collectionName+" properties");
+            subtest.end();
+        });
+    });
 });
