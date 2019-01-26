3b33f2e359bd3423fc02c2b5b9b0881e6f40a479
franck34 <franck.tab@gmail.com>
2019-01-26 11:09:33 +0100

    cosmetic

--- src/cli/usage.js
+++ src/cli/usage.js
@@ -1,8 +1,8 @@
 const use = require('abrequire');
 const ENV = use('src/env');
 
-console.log('Usage: loki-cli <url>');
+console.log('Usage: sloki-cli <url>');
 console.log();
 console.log('Examples:');
-console.log('   loki-cli tcp://localhost:'+ENV.NET_TCP_PORT);
-console.log('   loki-cli tls://localhost:'+ENV.NET_TCP_PORT);
+console.log('   sloki-cli tcp://localhost:'+ENV.NET_TCP_PORT);
+console.log('   sloki-cli tls://localhost:'+ENV.NET_TCP_PORT);
