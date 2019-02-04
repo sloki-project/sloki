67d95697c88bc8828ed76d8da97216feb8e30679
franck34 <franck.tab@gmail.com>
2019-02-04 12:48:29 +0100

    make eslint happy, remove require database

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const log = require('evillogger')({ns:'main'});
-const databases = require('./src/databases');
+const log = require('evillogger')({ ns:'main' });
 const server = require('./src/server');
 
 if (module === require.main) {
     server.start(() => {
-        log.info("server started");
+        log.info('server started');
     });
 }
 
 module.exports = {
     version:require('./package.json').version,
-    server:server
-}
+    server
+};
