b8925e8082dcbe687165b04cc94256c090cef278
franck34 <franck.tab@gmail.com>
2019-02-18 04:56:29 +0100

    avoid RESET server side

--- test/tests/400.collection.insert.js
+++ test/tests/400.collection.insert.js
@@ -68,7 +68,7 @@ require('./client')(__filename, (test, client) => {
             test.test('insert without callback (lazy mode)', (subtest)  => {
                 client.insert({ collection:collectionName, document: doc }, { lazy:true });
                 subtest.pass('pass');
-                subtest.end();
+                setTimeout(subtest.end, 100);
             });
         });
     });
