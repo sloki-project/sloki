72fa4eabc9d50edbbe150e3bee05d5cc174a70a9
franck34 <franck.tab@gmail.com>
2019-02-18 05:18:20 +0100

    display memory usage in test

--- test/tests/201.server.memory.js
+++ test/tests/201.server.memory.js
@@ -2,10 +2,7 @@ require('./client')(__filename, (test, client, end) => {
     test.test('memory', (subtest)  => {
         client.memory((err, result) => {
             subtest.deepEqual(err, undefined, 'method should not return an error');
-
-            // example
-            // { rss: '34.7 MB', heapTotal: '18.2 MB', heapUsed: '13.3 MB', external: '2.39 MB' }
-
+            subtest.pass(JSON.stringify(result));
             subtest.equal(typeof result.rss, 'string', 'rss should be a string');
             subtest.equal(typeof result.heapTotal, 'string', 'heapTotal should be a string');
             subtest.equal(typeof result.heapUsed, 'string', 'heapUsed should be a string');
