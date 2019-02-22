215394456abd385fc6d84ac3fa65103176b81012
franck34 <franck.tab@gmail.com>
2019-02-22 09:56:53 +0100

    improve cli help

--- src/cli/usage.js
+++ src/cli/usage.js
@@ -2,6 +2,15 @@ const config = require('../config');
 
 console.log('Usage: sloki-cli <url>');
 console.log();
-console.log('Examples:');
-console.log(`   sloki-cli tcp://localhost:${config.TCP_BINARY_PORT}`);
-console.log(`   sloki-cli tls://localhost:${config.TCP_BINARY_PORT}`);
+console.log('Examples with ports:');
+console.log(`sloki-cli tcp://localhost:${config.TCP_BINARY_PORT}`);
+console.log(`sloki-cli tls://localhost:${config.TLS_BINARY_PORT}`);
+console.log(`sloki-cli binary://localhost:${config.TCP_BINARY_PORT}`);
+console.log(`sloki-cli binarys://localhost:${config.TLS_BINARY_PORT}`);
+console.log(`sloki-cli jsonrpc://localhost:${config.TCP_JSONRPC_PORT}`);
+console.log(`sloki-cli jsonrpcs://localhost:${config.TLS_JSONRPC_PORT}`);
+console.log('');
+console.log('Notes:');
+console.log(' * tcp protocol is an alias for binary protocol');
+console.log(' * tls protocol is an alias for binarys protocol');
+console.log(' * you may not specify the port, in which case the default ports will be used.');
