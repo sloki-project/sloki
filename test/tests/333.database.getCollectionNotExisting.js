d9085846c5a8716b66e7a6d97dee3ce34f9d4350
franck34 <franck.tab@gmail.com>
2019-02-28 05:28:52 +0100

    fix test

--- test/tests/333.database.getCollectionNotExisting.js
+++ test/tests/333.database.getCollectionNotExisting.js
@@ -1,6 +1,7 @@
 const dbName = '__testAddCollectionWithOptions';
 const collectionName = 'myCollectionNotExisting';
 
+const expectedErr = { code: -32603, message: 'Collection does not exist' };
 
 require('./client')(__filename, (test, client, end) => {
     test.test('loadDatabase', subtest  => {
@@ -13,7 +14,7 @@ require('./client')(__filename, (test, client, end) => {
 
     test.test('getCollection', subtest  => {
         client.getCollection({ collection: collectionName }, (err, result) => {
-            subtest.deepEqual(err, undefined, 'method should not return an error');
+            subtest.deepEqual(err, expectedErr, `method should return error ${JSON.stringify(expectedErr)}`);
             subtest.deepEqual(result, undefined, 'should return undefined');
             subtest.end();
             end();
